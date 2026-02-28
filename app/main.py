908edcfce979d20edd7eb203df9161edacde0e14
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2026-02-28 22:56:34 +0200

    feat: complete project architecture with all developer classes and type hints

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ class FrontendDeveloper(SoftwareEngineer):
         super().__init__(name)
         self.skills.extend(["JavaScript", "HTML", "CSS"])
 
-    def create_awesome_web_page(self) -> None:
+    def create_awesome_web_page(self) -> str:
 
         print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
@@ -33,7 +33,7 @@ class BackendDeveloper(SoftwareEngineer):
         super().__init__(name)
         self.skills.extend(["Python", "SQL", "Django"])
 
-    def create_powerful_api(self) -> None:
+    def create_powerful_api(self) -> str:
 
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
@@ -46,10 +46,10 @@ class AndroidDeveloper(SoftwareEngineer):
         super().__init__(name)
         self.skills.extend(["Java", "Android studio"])
 
-    def create_smooth_mobile_app(self) -> None:
+    def create_smooth_mobile_app(self) -> str:
 
         print(f"{self.name} is creating a mobile app...")
-        return  "Ads every three swipes"
+        return "Ads every three swipes"
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
@@ -57,7 +57,6 @@ class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def __init__(self, name: str) -> None:
 
         super().__init__(name)
-
 
     def create_web_application(self) -> None:
 
