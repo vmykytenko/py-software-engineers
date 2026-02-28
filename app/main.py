ade9668787de14f347d46ab68a974f05cd151b92
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2026-02-28 21:11:43 +0200

    feat: design full class architecture and inheritance logic

--- app/main.py
+++ app/main.py
@@ -1,1 +1,52 @@
-# write your code here
+from __future__ import annotations
+
+
+class SoftwareEngineer:
+
+    def __init__(self, name: str) -> None:
+
+        pass
+
+    def learn_skill(self, skill: str) -> None:
+
+        pass
+
+
+class FrontendDeveloper(SoftwareEngineer):
+
+    def __init__(self, name: str) -> None:
+
+        pass
+
+    def create_awesome_web_page(self) -> None:
+
+        pass
+
+
+class BackendDeveloper(SoftwareEngineer):
+
+    def __init__(self, name: str) -> None:
+
+        pass
+
+    def create_powerful_api(self) -> None:
+
+        pass
+
+
+class AndroidDeveloper(SoftwareEngineer):
+
+    def __init__(self, name: str) -> None:
+
+        pass
+
+    def create_smooth_mobile_app(self) -> None:
+
+        pass
+
+
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+
+    def create_web_application(self) -> None:
+
+        pass
